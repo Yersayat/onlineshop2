52ef001951cd67667943efc0a62aa68809396dc3
yersayat <yersayat@gmail.com>
2023-05-23 21:49:38 +0600

    copy

---
+++ src/router/router.js
@@ -0,0 +1,26 @@
+import { createWebHistory, createRouter } from "vue-router";
+import vCatalog from "@/components/v-catalog.vue";
+import vCart from "@/components/v-cart.vue";
+
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'catalog',
+    component: vCatalog,
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: vCart,
+    props: true
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
